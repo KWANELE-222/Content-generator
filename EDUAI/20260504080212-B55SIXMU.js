805369ce051e5201eb649a212ec81d2e2c62b18d
Kwanele Sekhoto <kmpefela@gmail.com>
2026-05-19 14:48:02 +0200

    Add files via upload

---
+++ EDUAI/20260504080212-B55SIXMU.js
@@ -0,0 +1,20 @@
+window.botpress.init({
+  "botId": "49165123-5810-4ed6-8326-a302859ce537",
+  "configuration": {
+    "version": "v1",
+    "website": {},
+    "email": {},
+    "phone": {},
+    "termsOfService": {},
+    "privacyPolicy": {},
+    "color": "#3276EA",
+    "variant": "solid",
+    "headerVariant": "glass",
+    "themeMode": "light",
+    "fontFamily": "inter",
+    "radius": 4,
+    "feedbackEnabled": false,
+    "footer": "[⚡ by Botpress](https://botpress.com/?from=webchat)"
+  },
+  "clientId": "896515a3-b530-4f8c-a516-c1c5062c6242"
+});
